eddf812274410101854a1d570d074ea29b678aa3
Mati <matiborlak@gmail.com>
2017-04-25 15:11:02 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Guy Oren</h2>
+        </div>
+        <p className="App-intro">
+          Lets get this show on the road please.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
