e0fe7315b567a84b45f8a01b27e734aee6cbfbad
assafzarfaty <zarfaty@blipitapp.com>
2017-04-25 18:43:38 +0300

    starting out design in react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Guy Oren</h2>
-        </div>
-        <p className="App-intro">
-          Lets get this show on the road please.
-        </p>
 
-      </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 navbar">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <hr/>
+              <div className="col-md-12 navbar">
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+
+          </div>
+        </div>
+
+
+
     );
   }
 }
