123bb9a5262860efeb49adaa39a9f718e20f7c40
assafzarfaty <zarfaty@blipitapp.com>
2017-04-25 20:10:51 +0300

    input

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
               <input type ="text" className="inputDomain" placeholder="Domain Name"></input>
             </div>
             <div className="col-md-6 ">
-              <p className="intro"> zendesk.com</p>
+              <p className="intro">zendesk.com</p>
             </div>
           </div>
           </div>
