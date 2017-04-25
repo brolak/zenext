c058d1deaf51aee81f2d56700500114df82083c6
talpod <talpod@gmail.com>
2017-04-25 18:43:57 +0300

    menifest with permissions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+
+
+
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Guy Oren</h2>
+          <h2>Guy Oren sucks</h2>
         </div>
         <p className="App-intro">
           Lets get this show on the road please.
@@ -18,5 +24,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
