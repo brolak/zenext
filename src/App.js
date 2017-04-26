1e7b1d442f1136fefd598f46246e74eaafd53be7
assafzarfaty <zarfaty@blipitapp.com>
2017-04-26 09:30:42 +0300

    added function to change the state new tasks

--- src/App.js
+++ src/App.js
@@ -16,12 +16,27 @@ class App extends Component {
   }
   handleSignIn = (e) => {
     e.preventDefault();
-    console.log("hello")
+    console.log("domain:" , this.state.domainName)
+    axios.get('https://zenext.zendesk.com/api/v2/search.json?query=type:ticket%20status:new')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
   handleInput = (e) => {
     this.setState({
       domainName: e.target.value
     })
+  }
+
+  componentDidMount = () => {
+    this.state
+    setInterval( (x)=> {
+
+      this.setState({newTasks:this.state.newTasks+=1})
+    }, 5000);
   }
 
 
