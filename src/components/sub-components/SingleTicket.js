eb536cf9f2720c6caf5982a291eba4e414e407b8
assafzarfaty <zarfaty@blipitapp.com>
2017-05-01 01:55:38 +0300

    design text

--- src/components/sub-components/SingleTicket.js
+++ src/components/sub-components/SingleTicket.js
@@ -11,11 +11,11 @@ class SingleTicket extends Component {
         return (
             <div class="ticket" onClick={this.click}>
                 <table className="tickets">
-                    <tr>
+                    <tr className="upper-title">
                         <td className="ticketSubject align-left">{this.props.subject}</td>
                         <td className="ticketRequester align-right">{this.props.requester}</td>
                     </tr>
-                    <tr>
+                    <tr className="lower-desc">
                         <td className="ticketDesc align-left">{this.props.description}</td>
                         <td className="ticketTime align-right"><TimeAgo date={this.props.created_at}/></td>
                     </tr>
