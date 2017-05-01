38ebba4e724c5e01098f2dc2ec22631071cc9a1f
talpod <talpod@gmail.com>
2017-05-01 10:50:55 +0300

    notification toggle 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import preloader from './assets/preloader.gif';
 
 import settings from './assets/settings.png';
 import exit from './assets/exit.png'
+import Toggle from 'react-toggle'
+import Heart from './assets/heart.png';
 
 import axios from 'axios';
 import Tickets from './components/Tickets';
@@ -83,6 +85,7 @@ class App extends Component {
         this.setState({userStatus: 2})
     }
 
+
     settings = () => {}
 
     render() {
@@ -97,8 +100,10 @@ class App extends Component {
                                 <tr>
                                     <td className="align-left"><img src={logo} className="App-logo" alt="logo"/></td>
                                     <td className="align-right">
-                                        <a href=""><img src={settings} className="settings-logo" alt="settings"/></a>&nbsp;&nbsp;&nbsp;
-                                        <a href=""><img src={exit} className="exit-logo" alt="Sign out" onClick={this.logout}/></a>
+
+                                      <Toggle defaultChecked={this.state.notifyme} onChange={this.handleBaconChange} />
+                                      &nbsp;&nbsp;&nbsp;
+                                      <a href=""><img src={exit} className="exit-logo" alt="Sign out" onClick={this.logout}/></a>
                                     </td>
                                 </tr>
                             </table>
