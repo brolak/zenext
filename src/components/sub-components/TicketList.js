fdd2962983b9468500f5870ea6d34b5d46f48e43
Guy Oren <only1guy@gmail.com>
2017-05-03 16:07:39 +0300

    Counter change

--- src/components/sub-components/TicketList.js
+++ src/components/sub-components/TicketList.js
@@ -4,19 +4,29 @@ import SingleTicket from './SingleTicket';
 class TicketList extends Component {
     render() {
 
-        let items = this.props.tickets.map((ticket)=> {
+        let items = this.props.tickets.map((ticket) => {
             return (<SingleTicket domain={this.props.domain} key={ticket.ticket.id} description={ticket.ticket.description} subject={ticket.ticket.subject} status={ticket.ticket.status} id={ticket.ticket.id} requester_id={ticket.requester_id} created_at={ticket.created} requester="assaf"/>)
         })
-        return (
-            <div>
-                <div className="row">
-                    <div className="ticketList">
-                        {items}
+
+        if (this.props.newTickets == 0) {
+            return (
+                <div>
+                  <br />
+                    <span text-align="center">No tickets - all good :)</span>
+                </div>
+            )
+        } else {
+
+            return (
+                <div>
+                    <div className="row">
+                        <div className="ticketList">
+                            {items}
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
-
 export default TicketList;
