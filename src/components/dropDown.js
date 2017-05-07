218d892b02431b1181fbb2096331e37244850d9c
Guy Oren <only1guy@gmail.com>
2017-05-07 20:31:57 +0300

    spachtel

--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -2,31 +2,31 @@ import React, {Component} from 'react';
 
 class DropDown extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { value: this.props.defaultViewTitle};
+        this.state = {
+            value: this.props.defaultViewTitle
+        };
 
-  }
+    }
 
-  onChange(e) {
-    this.setState({
-      value: e.target.value
-    })
-    this.props.changeView(e.target.value)
-  }
+    onChange(e) {
+        this.setState({value: e.target.value})
+        this.props.changeView(e.target.value)
+    }
     render() {
-      console.log("current view" ,this.state.value)
-      return (
-        <div className="form-group">
-          <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
-            {this.props.viewsArr.map(option => {
-              return <option value={option.title} id={option.id} key={option.id}>{option.title}</option>
-            })}
-          </select>
-        </div>
+        console.log("current view", this.state.value)
+        return (
+            <div className="form-group">
+                <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
+                    {this.props.viewsArr.map(option => {
+                        return <option value={option.title} id={option.id} key={option.id}>{option.title}</option>
+                    })}
+                </select>
+            </div>
 
-  )
+        )
     }
 }
 
