d8f85d5701303adb0ea7becc4f37e6219b57b21f
assafzarfaty <zarfaty@blipitapp.com>
2017-05-07 20:31:19 +0300

    view list design

--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -18,7 +18,7 @@ class DropDown extends Component {
     render() {
       console.log("current view" ,this.state.value)
       return (
-        <div className="form-group">
+        <div className="views">
           <select value={this.state.value} onChange={this.onChange.bind(this)} className="form-control">
             {this.props.viewsArr.map(option => {
               return <option value={option.title} id={option.id} key={option.id}>{option.title}</option>
